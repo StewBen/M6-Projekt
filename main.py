93db909daea927325128032fc6f16236654bbb56
StoicAurora <viktor.stubbfalt@gmail.com>
2021-10-07 01:32:45 +0200

    Lade till fler kommentarer

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from grid import Grid
 ### Global variables:
 SCREEN_HEIGHT = 720
 SCREEN_WIDTH = 720
-FPS = 60
 SPEED = 100 # Milliseconds between each step of snake movement
 WIDTH = 18 # Width and height of all squares ingame
 GRID_SIZE = SCREEN_WIDTH // WIDTH # Amount of squares for one side
@@ -17,10 +16,10 @@ GRID_SIZE = SCREEN_WIDTH // WIDTH # Amount of squares for one side
 def main(screen):
     """Main-function for the game."""
 
-    pygame.init()               # Initiates pygame
-    program_running = True      # Is the program running or not
-    grid = Grid(GRID_SIZE, WIDTH)          # Initates the grid variable with black colors
-    player_1 = Snake(20, 20, 1) # Player 1
+    pygame.init()                 # Initiates pygame
+    program_running = True        # Is the program running or not
+    grid = Grid(GRID_SIZE, WIDTH) # Initates the grid variable with black colors
+    player_1 = Snake(20, 20, 1)   # Player 1
 
     ### Sets up an event every SPEED ms to move the snakes:
     move_event = pygame.USEREVENT
@@ -33,6 +32,7 @@ def main(screen):
         keys = pygame.key.get_pressed()
         player_1.change_direction(keys)
 
+        ### Checks for game events:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # If you close the window
                 pygame.quit()             # Quits the program
@@ -41,12 +41,13 @@ def main(screen):
             if event.type == move_event:  # Every SPEED ms:
                 player_1.move()           # Move the snakes
 
-        ### Resets, updates, and draws the playing field:
+        ### Resets, updates, and then draws the playing field:
         grid.reset()
         grid.update(player_1)
         grid.draw(screen)
 
-        pygame.display.flip() # The *actual* drawing of this frame
+        ### The *actual* drawing of this frame:
+        pygame.display.flip()
 
 ### Initiate a "display variable" to draw things on:
 display_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
