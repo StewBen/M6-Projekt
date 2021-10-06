2a36986c58ac1ed3a2efef0913096804b05319f7
StoicAurora <viktor.stubbfalt@gmail.com>
2021-10-07 00:00:29 +0200

    Lade till funktionen draw_grid samt kommentarer.

--- main.py
+++ main.py
@@ -1,36 +1,57 @@
-"""Mall för ett pygame spel."""
+"""Snake-Duel, a 1v1 multiplayer spin on the classic game Snake.
+   Created for project module M6 by Viktor Stubbfält and Einar Johansson."""
 
 ### Imports:
+from random import randint
 import pygame
 
-### Program parameters:
+### Global variables:
 SCREEN_HEIGHT = 720
 SCREEN_WIDTH = 720
 FPS = 60
+WIDTH = 18
+GRID_SIZE = SCREEN_WIDTH // WIDTH
+BLACK = (0,0,0)
+
+def draw_grid(screen, grid):
+    """Draws the contents of the playing field."""
+
+    for x, column in enumerate(grid):
+        for y, row in enumerate(column):
+            square = pygame.Rect(x * WIDTH + (x * WIDTH) % 18, # Left edge position
+                                 y * WIDTH + (y * WIDTH) % 18, # Top edge position
+                                 WIDTH,                        # Width
+                                 WIDTH)                        # Height
+            pygame.draw.rect(screen, row, square)              # Does the drawing
 
 def main(screen):
-    """Main-funktion för spelet."""
+    """Main-function for the game."""
 
-    pygame.init()               # Initierar pygame
-    program_running = True      # Om programmet körs eller ej
-    clock = pygame.time.Clock() # Tidsvariabel för FPS t.ex.
+    pygame.init()               # Initiates pygame
+    program_running = True      # Is the program running or not
+    clock = pygame.time.Clock() # Time variable
 
-    ### Vår main-loop, varje iteration av loop:en = 1 frame:
+    ### Initates the grid variable with either black or random colors:
+    #grid = [[BLACK for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+    grid = [[(randint(0,255),randint(0,255),randint(0,255)) for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+
+    ### Our main-loop, every iteration of the loop = 1 frame:
     while program_running:
 
-        ### Kollar efter mus/tangenttryck:
+        ### Checks for mouse/keypresses:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: # Om man kryssar fönstret
-                pygame.quit()             # Stänger programmet
-                return None               # Löser en märklig bugg
+            if event.type == pygame.QUIT: # If you close the window
+                pygame.quit()             # Quits the program
+                return None               # Solves odd error
 
-        screen.fill((0,0,0))  # Ritar svart bakgrund
+        draw_grid(screen, grid) # Draws the playing field
 
-        clock.tick(FPS)       # Sätter vår target FPS
-        pygame.display.flip() # Faktiska ritningen av denna frame
+        clock.tick(FPS)       # Sets our target FPS
+        pygame.display.flip() # The *actual* drawing of this frame
 
-### Initiera en "displayvariabel" att rita saker på:
+### Initiate a "display variable" to draw things on:
 display_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('') # Fönstrets titel
+pygame.display.set_caption('Snake Duel') # The title of the window
 
-main(display_window)           # Startar spelet
+### Starts the game!
+main(display_window)
