8eee2055c624895baa5df0366007ea84e8b93c2c
Einar Johansson <ejrobb09@gmail.com>
2021-10-12 10:05:10 +0200

    Gjorde grid.data till 2d lista med squares.

--- grid.py
+++ grid.py
@@ -3,38 +3,36 @@
 ### Imports:
 import pygame
 from global_parameters import WHITE, BLACK
-
+from square import Square
 class Grid:
     """Color data stored in a grid."""
     def __init__(self, squares, width, size):
         self.squares = squares
         self.width = width
         self.size = size
-        self.data = [[BLACK for _ in range(self.squares)] for _ in range(self.squares)]
+        
+        self.reset()
 
     def reset(self):
         """Resets all grid data to black color."""
-        self.data = [[BLACK for _ in range(self.squares)] for _ in range(self.squares)]
-
+        self.data = [[Square(col * self.width, row * self.width, BLACK) for col in range(self.squares)] for row in range(self.squares)]
+    
     def update(self, player_1):
         """Updates the data of grid."""
-        self.data[player_1.x][player_1.y] = player_1.color
-        self.data[player_1.apple.x][player_1.apple.y] = player_1.apple.color
+
+        self.data[player_1.x][player_1.y] = player_1
+        self.data[player_1.apple.x][player_1.apple.y] = player_1.apple
         for tail in player_1.tails:
-            self.data[tail.x][tail.y] = tail.color
+            self.data[tail.x][tail.y] = tail
 
     def draw_squares(self, screen):
         """Draws the squares of the playing field."""
         for x, column in enumerate(self.data):
-            for y, row in enumerate(column):
-                square = pygame.Rect(
-                         x * self.width + (x * self.width) % self.width, # Left edge position
-                         y * self.width + (y * self.width) % self.width, # Top edge position
-                         self.width, self.width)                         # Width, height
-                pygame.draw.rect(screen, row, square)                    # Draws the square
+            for y, row in enumerate(column):                
+                pygame.draw.rect(screen, row.color, row.rect(y, x)) # Draws the square
 
     def draw_lines(self, screen):
         """Draws grid lines on the playing field."""
-        for i, _ in enumerate(self.data):
+        for i, col in enumerate(self.data):
             pygame.draw.line(screen, WHITE, (i * self.width, 0), (i * self.width, self.size))
             pygame.draw.line(screen, WHITE, (0, i* self.width), (self.size, i * self.width))
--- square.py
+++ square.py
@@ -1,6 +1,16 @@
+import pygame
+
+from global_parameters import WIDTH
+
 class Square:
     '''A square in the grid.'''
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = color
+        self.color = color   
+
+    def rect(self, row, column):
+        return pygame.Rect(
+                         column * WIDTH + (column * WIDTH) % WIDTH, # Left edge position
+                         row * WIDTH + (row * WIDTH) % WIDTH,       # Top edge position
+                         WIDTH, WIDTH)                              # Width, height
