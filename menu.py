f0d69ecc744021ff8699d04095f55ff0da2a0dba
StoicAurora <viktor.stubbfalt@gmail.com>
2021-10-14 13:28:12 +0200

    temporarily disabled game over

--- menu.py
+++ menu.py
@@ -4,7 +4,8 @@ import pygame
 from global_parameters import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def gameover(screen):
-    myfont = pygame.font.SysFont('Comic Sans MS', 60)
-    textsurface = myfont.render('Game Over', True, (255, 0, 0))
-    screen.blit(textsurface,((SCREEN_WIDTH - pygame.Surface.get_width(textsurface)) // 2,
-                              (SCREEN_HEIGHT - pygame.Surface.get_height(textsurface)) // 2))
+    pass
+    # myfont = pygame.font.SysFont('Comic Sans MS', 60)
+    # textsurface = myfont.render('Game Over', True, (255, 0, 0))
+    # screen.blit(textsurface,((SCREEN_WIDTH - pygame.Surface.get_width(textsurface)) // 2,
+    #                           (SCREEN_HEIGHT - pygame.Surface.get_height(textsurface)) // 2))
