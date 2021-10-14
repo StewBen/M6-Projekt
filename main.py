7cb1b5014c870db4254db184bae5f70f05c105dd
StoicAurora <viktor.stubbfalt@gmail.com>
2021-10-14 14:05:21 +0200

    Added acceleration

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 from snake import Snake
 from grid import Grid
 from menu import gameover
-from global_parameters import SCREEN_WIDTH, SCREEN_HEIGHT, WIDTH, GRID_SIZE, SPEED
+from global_parameters import SCREEN_WIDTH, SCREEN_HEIGHT, WIDTH, GRID_SIZE
 
 def main(screen):
     """Main-function for the game."""
@@ -17,12 +17,20 @@ def main(screen):
     grid = Grid(GRID_SIZE, WIDTH, SCREEN_WIDTH) # Initates the grid variable with black colors
     player_1 = Snake(12, 12, 1)   # Player 1
 
-    ### Sets up an event every SPEED ms to move the snakes:
-    move_event = pygame.USEREVENT
-    pygame.time.set_timer(move_event, SPEED)
+    SPEED = 300
+    ACCELERATION = 5000
+    clock = pygame.time.Clock()
+    stopwatch = 0
+
+    ### Sets up an event every ACCELERATION ms to increase snake speed:
+    accelerate_event = pygame.USEREVENT
+    pygame.time.set_timer(accelerate_event, ACCELERATION)
 
     ### Our main-loop, every iteration of the loop = 1 frame:
     while program_running:
+
+        ### Adds time since last frame to stopwatch:
+        stopwatch += clock.get_time()
 
         ### Checks for keypresses:
         keys = pygame.key.get_pressed()
@@ -34,20 +42,26 @@ def main(screen):
                 pygame.quit()             # Quits the program
                 return None               # Solves odd error
 
-            if event.type == move_event:  # Every SPEED ms:
-                player_1.move()           # Move the snakes
-                if player_1.wall_collision() or player_1.tail_collision():
-                    gameover(screen)
-                    break
+            ### Accelerates the snake:
+            if event.type == accelerate_event:
+                SPEED -= 10
 
-                ### Resets, updates, and then draws the playing field:
-                grid.reset()
-                grid.update(player_1)
-                grid.draw_squares(screen)
-                grid.draw_lines(screen)
+        if stopwatch > SPEED: # Every SPEED ms:
+            player_1.move()   # Move the snakes
+            stopwatch = 0     # Reset stopwatch
+            if player_1.wall_collision() or player_1.tail_collision():
+                gameover(screen)
+                break
+
+            ### Resets, updates, and then draws the playing field:
+            grid.reset()
+            grid.update(player_1)
+            grid.draw_squares(screen)
+            grid.draw_lines(screen)
 
         ### The *actual* rendering of this frame:
         pygame.display.flip()
+        clock.tick(60)
 
 ### Initiate a "display variable" to draw things on:
 display_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
